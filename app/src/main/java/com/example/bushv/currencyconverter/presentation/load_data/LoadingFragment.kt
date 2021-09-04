c089dd083d11c57b40c3eabaebff92c3e52cd545
Vadim Bush <bushmvadim@gmail.com>
2021-09-04 15:45:09 +0300

    Join loadingViewModel to LoadingFragment

--- app/src/main/java/com/example/bushv/currencyconverter/presentation/load_data/LoadingFragment.kt
+++ app/src/main/java/com/example/bushv/currencyconverter/presentation/load_data/LoadingFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
 import com.example.bushv.currencyconverter.R
 import com.example.bushv.currencyconverter.databinding.FragLoadingBinding
 
 class LoadingFragment: Fragment() {
 
     private lateinit var binding: FragLoadingBinding
+    private val viewModel by viewModels<LoadingViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +26,16 @@ class LoadingFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.loadingResult.observe(viewLifecycleOwner) {
+            when(it) {
+                is Result.Error -> showErrorLoadingMessage()
+                is Result.Success ->  {}// TODO: navigate to converter fragment
+            }
+        }
+    }
+
+    private fun showErrorLoadingMessage() {
+        Toast.makeText(requireContext(), getString(R.string.loading_error_message), Toast.LENGTH_LONG).show()
     }
 
 }
