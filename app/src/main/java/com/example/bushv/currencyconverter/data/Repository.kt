71bbcad945673d215d787d470580d8bdb985c312
Vadim Bush <bushmvadim@gmail.com>
2021-09-04 15:46:22 +0300

    Add base implementation for Repository

---
+++ app/src/main/java/com/example/bushv/currencyconverter/data/Repository.kt
@@ -0,0 +1,7 @@
+package com.example.bushv.currencyconverter.data
+
+import com.example.bushv.currencyconverter.domain.Currency
+
+object Repository {
+    val currencyMap: MutableMap<String, Currency> = HashMap()
+}
